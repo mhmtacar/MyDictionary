d9f1ba8b0b63c959e88fb7a9916f0ecb1f5d90b2
Mehmet <mehmet-acar-gs@hotmail.com>
2024-03-23 23:12:05 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<Key,Value>
+    {
+        Key[] keys;
+        Value[] values;
+
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+
+        public void Add(Key key, Value value)
+        {
+            Key[] tempKeys = keys;
+            Value[] tempValues = values;
+
+            keys = new Key[keys.Length+1];
+            values = new Value[values.Length+1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length-1] = key;
+            values[values.Length-1] = value;
+
+        }
+
+        public Key[] Keys {
+            get {  return keys; }
+        }
+        public Value[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyDictionary;
+
+MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+
+myDictionary.Add(1, "Mehmet");
+myDictionary.Add(2, "Ali");
+
+int[] keys = myDictionary.Keys;
+string[] values = myDictionary.Values;
+
+for (int i = 0; i < keys.Length; i++)
+{
+    Console.WriteLine("Key: {0} Value: {1}", keys[i], values[i]);
+}
